4ccfd141b04ed678a7c9937d3d06c307b81328e9
Alessandro <ale.lugari@gmail.com>
2021-05-30 22:48:28 +0200

    aggiunta font_size

--- pedestrian/inference.py
+++ pedestrian/inference.py
@@ -11,9 +11,9 @@ import os
 
 WEIGHT_PATH = os.getenv("WEIGHT_PATH")
 ANNOTATION_FILE = os.getenv("ANNOTATION_FILE")
-
-SCORE_THRESHOLD = 0.8
-FONT_SIZE = 100
+FONT_PATH = os.getenv("FONT_PATH")
+SCORE_THRESHOLD = 0.2
+FONT_SIZE = 20
 FOCAL_LENGTH = 35
 OBJECT_HEIGHT = 1700
 
@@ -54,7 +54,7 @@ plt.show()
 for i, box in enumerate(boxes):
     print(box)
     if scores[i] > SCORE_THRESHOLD:
-        fnt = ImageFont.truetype(size=FONT_SIZE)
+        fnt = ImageFont.truetype(font=FONT_PATH, size=FONT_SIZE)
         draw.rectangle(((box[0], box[1]), (box[2], box[3])), outline="red", width=5)
         distance = (FOCAL_LENGTH * OBJECT_HEIGHT) / (box[3] - box[1])
         draw.text((box[0], box[1]), f"distance: {( distance / 10)}", font=fnt)
