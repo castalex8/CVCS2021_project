91b390fa1785ea6e203bb36d7a04608d03cd2f26
Alessandro <ale.lugari@gmail.com>
2021-05-31 20:30:57 +0200

    added relu layers

--- pedestrian/detection/classifier.py
+++ pedestrian/detection/classifier.py
@@ -6,20 +6,23 @@ class Classifier(nn.Module):
     def __init__(self, input_channel, num_classes):
         super(Classifier, self).__init__()
         # classification layers (for scores)
-        self.classification_layer_1 = nn.Linear(input_channel, 15)
-        self.classification_layer_2 = nn.Linear(15, 15)
-        self.classification_layer_3 = nn.LayerNorm(15, 2)
-
+        self.classification = nn.Sequential(
+            nn.Linear(input_channel, 15),
+            nn.ReLU(),
+            nn.Linear(15, 15),
+            nn.ReLU(),
+            nn.LayerNorm(15, 2)
+        )
         # regression layers (for bounding boxes)
-        self.regression_layer_1 = nn.Linear(input_channel, 15)
-        self.regression_layer_2 = nn.Linear(15, 15)
-        self.regression_layer_3 = nn.LayerNorm(15, num_classes * 4)
+        self.regression = nn.Sequential(
+            nn.Linear(input_channel, 15),
+            nn.ReLU(),
+            nn.Linear(15, 15),
+            nn.ReLU(),
+            nn.LayerNorm(15, num_classes * 4)
+        )
 
     def forward(self, x):
-        scores = self.classification_layer_3(
-            self.classification_layer_2(self.classification_layer_1(x))
-        )
-        bbox_coord = self.regression_layer_3(
-            self.regression_layer_2(self.regression_layer_1(x))
-        )
+        scores = self.classification(x)
+        bbox_coord = self.regression(x)
         return scores, bbox_coord
