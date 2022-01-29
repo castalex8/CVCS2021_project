6af8cbe906a578aefa3ea8c23c90bf673eb01207
Matteo Corradini <corra.matteo@gmail.com>
2022-01-29 12:36:23 +0100

    Bug fixes on weights and typing

--- road_signs/datasets_utils.py
+++ road_signs/datasets_utils.py
@@ -20,7 +20,7 @@ datasets = {
         'transform': MAPILLARY_TRANSFORM,
         'dataset': MapillaryDatasetAbs(train=True),
         'class_weights': '0021.pth',
-        'retrieval_siamese_weights': '0015.pth',
+        'retrieval_siamese_weights': '0013.pth',
         'retrieval_triplet_weights': '0017.pth',
         'get_images': lambda x: x.labels,
         'get_image': lambda x: x,
